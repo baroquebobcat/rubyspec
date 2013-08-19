00088a46c4c71c5661b34fa08da918449d8b4c65
Nick Howard <ndh@baroquebobcat.com>
2013-08-19 12:54:30 -0600

    add tests for thread variable behavior when the thread is frozen

--- core/thread/thread_variable_set_spec.rb
+++ core/thread/thread_variable_set_spec.rb
@@ -9,6 +9,19 @@ ruby_version_is "2.0" do
       lambda { Thread.current.thread_variable_set(5, true) }.should raise_error(TypeError)
     end
 
+    it "raises a RuntimeError when a local has been set and the thread is frozen" do
+      t = Thread.new { }.join
+      t.thread_variable_set :foo, "bar"
+      t.freeze
+      lambda { t.thread_variable_set(:baz, "qux") }.should raise_error(RuntimeError)
+    end
+
+    it "raises a RuntimeError when locals are empty and the thread is frozen" do
+      t = Thread.new { }.join
+      t.freeze
+      lambda { t.thread_variable_set(:baz, "qux") }.should raise_error(RuntimeError)
+    end
+
     it "is shared across fibers" do
       fib = Fiber.new do
         Thread.current.thread_variable_set(:value, 1)
